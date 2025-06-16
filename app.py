e4f5faf93ac4d4b407a0f05f5afbae7c88bf3605
Jose Arthur <arthurbarros971@gmail.com>
2025-06-16 20:46:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,20 @@
-from flask import Flask, request, redirect
+from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Chatbot ativo!"
+    return "Bot está ativo no Render!"
 
 @app.route("/sms", methods=["POST"])
 def sms_reply():
-    """Responde a mensagens SMS com uma mensagem simples"""
-    msg = request.form.get('Body')
-
-    # Criar resposta
+    msg = request.form.get("Body")
     resp = MessagingResponse()
     resp.message(f"Você disse: {msg}")
-
     return str(resp)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
